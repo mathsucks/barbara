5fd2c54800693234db9b53dee87f91592db2470f
M. de Verteuil <mverteuil@users.noreply.github.com>
2018-04-28 13:11:32 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+        name='barbara',
+        version='0.1',
+        packages=['barbara'],
+        url='http://github.com/mverteuil/barbara',
+        license='GNU General Public License v3.0',
+        author='Matthew de Verteuil',
+        author_email='mverteuil@github.com',
+        description='Environment variable management'
+)
