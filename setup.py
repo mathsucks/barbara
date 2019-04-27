90c02da3b87e0fe8802f17b697f8b29c7dace4fa
Matthew de Verteuil <onceuponajooks@gmail.com>
2019-04-27 18:15:17 -0400

    bump version to 0.10.0

--- setup.py
+++ setup.py
@@ -13,25 +13,26 @@ import os.path
 
 readme = ''
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.rst'), encoding='utf-8') as stream:
-    readme = stream.read()
+with open(os.path.join(here, 'README.rst'), 'rb') as stream:
+    readme = stream.read().decode('utf8')
 
 setup(
     long_description=readme,
     name='barbara',
-    version='0.9.6',
+    version='0.10.0',
     description='Environment variable management',
     python_requires='==3.*,>=3.6.0',
     author='Matthew de Verteuil',
     author_email='onceuponajooks@gmail.com',
     license='GPL-3.0',
-    classifiers=('Development Status :: 4 - Beta', 'Environment :: Console',
-                 'Intended Audience :: Developers',
-                 'Intended Audience :: System Administrators',
-                 'Operating System :: MacOS',
-                 'Operating System :: POSIX :: Linux',
-                 'Operating System :: Unix', 'Topic :: Software Development',
-                 'Topic :: System', 'Topic :: Terminals'),
+    classifiers=[
+        'Development Status :: 4 - Beta', 'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: MacOS', 'Operating System :: POSIX :: Linux',
+        'Operating System :: Unix', 'Topic :: Software Development',
+        'Topic :: System', 'Topic :: Terminals'
+    ],
     entry_points={
         'console_scripts': [
             'barb = barbara.cli:barbara_develop',
@@ -41,16 +42,16 @@ setup(
     packages=['barbara'],
     package_data={},
     install_requires=[
-        'boto3==1.7.48', 'click==7.*,>=7.0.0', 'poetry-setup==0.*,>=0.3.6',
-        'poetry-version==0.*,>=0.1.3', 'python-dotenv==0.*,>=0.10.1',
-        'pyyaml==5.*,>=5.1.0'
+        'boto3==1.7.48', 'click==7.*,>=7.0.0', 'poetry-version==0.*,>=0.1.3',
+        'python-dotenv==0.*,>=0.10.1', 'pyyaml==5.*,>=5.1.0'
     ],
     extras_require={
         'dev': [
-            'black==19.3b0', 'dephell==0.*,>=0.3.1', 'flake8==3.*,>=3.7.0',
-            'flake8-bugbear==19.*,>=19.3.0', 'flake8-mypy==17.*,>=17.8.0',
-            'flake8-polyfill==1.*,>=1.0.0', 'isort[pyproject]==4.*,>=4.3.0',
-            'lockfile==0.*,>=0.12.2', 'pre-commit==1.*,>=1.15.0',
+            'black==19.3b0', 'dephell[full]==0.*,>=0.5.8',
+            'flake8==3.*,>=3.7.0', 'flake8-bugbear==19.*,>=19.3.0',
+            'flake8-mypy==17.*,>=17.8.0', 'flake8-polyfill==1.*,>=1.0.0',
+            'isort[pyproject]==4.*,>=4.3.0', 'lockfile==0.*,>=0.12.2',
+            'poetry-setup==0.*,>=0.3.6', 'pre-commit==1.*,>=1.15.0',
             'pytest==4.*,>=4.4.0', 'pytest-cov==2.*,>=2.6.0'
         ]
     },
