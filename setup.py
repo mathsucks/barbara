da444f048b7038c0802cefcd68ab5038155dca2f
Matthew de Verteuil <onceuponajooks@gmail.com>
2019-05-21 23:00:20 -0400

    Pip version update, 1.1.1 version update

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(os.path.join(here, 'README.rst'), 'rb') as stream:
 setup(
     long_description=readme,
     name='barbara',
-    version='1.1.0',
+    version='1.1.1',
     description='Environment variable management',
     python_requires='==3.*,>=3.6.0',
     author='Matthew de Verteuil',
